f8248f8506c9d8d8f7940d901a229988b85cbdea
YINGMEI <j.yeongmee@gmail.com>
2023-11-13 21:46:59 +0900

    구글 로그인 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,10 +33,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> signInWithGoogle(BuildContext context) async {
+    GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignInAccount? account = await googleSignIn.signIn();
+    //TODO: account에서 가져온 정보 중 sparcs 계정 필터링 해서 로그인 시키기
+    if (account != null) {
+      GoogleSignInAuthentication authentication = await account.authentication;
+      OAuthCredential googleCredential = GoogleAuthProvider.credential(
+        idToken: authentication.idToken,
+        accessToken: authentication.accessToken,
+      );
+      UserCredential credential =
+          await _firebaseAuth.signInWithCredential(googleCredential);
+      if (credential.user != null) {
+        var user = credential.user;
+        print(user);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: TextButton(
+        onPressed: () async {
+          signInWithGoogle(context);
+        },
+        child: const Center(child: Text("구글 로그인")),
+      ),
     );
   }
 }
